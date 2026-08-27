c59141dba450a1b345883848c6b3ff6e0de297f4
Neha Hombal <104883080+NehaHombal@users.noreply.github.com>
2026-08-27 23:12:30 +0530

    need code fix
    Added a word list and implemented the game logic for Hangman.

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,35 @@
-print("Welcome to HangMan!!..")
+import random
+print("Hi Welcome to Hangman!..")
+words = [
+    "ghost",
+    "jungle",
+    "puzzle",
+    "volcano",
+    "elephant",
+    "knight",
+    "rainbow",
+    "dinosaur",
+    "adventure",
+    "butterfly"
+]
+word = random.choice(words)
+display = ["_"] * len(word)
+
+lives = 10
+while lives > 0:
+    print("Can you guess the letters of :" + str(display))
+    letter = input("Type in your letter :")
+
+    for i, char in enumerate(word):
+        if letter == char:
+            display[i] = letter
+            # print("Correct " + str(display))
+        else:
+            lives -= 1
+            # print("Wrong " + str(display))
+
+    if "_" not in display:
+        print("You won!")
+    else:
+        print("You lose!")
+        print("The word was :" + word)
